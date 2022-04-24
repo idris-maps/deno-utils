5282a9b304550f6fb44222d57c6b0833a45282d2
Anders <idris.maps@gmx.ch>
2022-04-24 22:32:11 +0200

    Bump server-cli version

--- server-cli/lib/deps.ts
+++ server-cli/lib/deps.ts
@@ -1,1 +1,1 @@
-export type { Endpoint } from "https://deno.land/x/anders@v0.0.3/server/types.d.ts";
+export type { Endpoint } from "https://deno.land/x/anders@v0.0.8/server/types.d.ts";
--- server-cli/mod.ts
+++ server-cli/mod.ts
@@ -3,7 +3,7 @@ import createRoute from "./lib/create-route/mod.ts";
 import generateRoutesFile from "./lib/generate-routes-file/mod.ts";
 import * as msg from "./lib/msg.ts";
 
-const UTILS_VERSION = "v0.0.7";
+const UTILS_VERSION = "v0.0.8";
 
 const [op, ...args] = Deno.args;
 
