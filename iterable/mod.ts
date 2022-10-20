2fed68926b2b1249d674052f2631678dee640f49
Anders <idris.maps@gmx.ch>
2022-10-20 20:46:42 +0200

    fix types

--- iterable/mod.ts
+++ iterable/mod.ts
@@ -1,13 +1,12 @@
 export { pipe } from "./pipe.js";
 export { fromFile } from "./from-file.ts";
 
-export type Iter<T> = AsyncGenerator<T> | AsyncIterable<T>;
-export type IterFunc<A, B> =
+export type Func<A, B> =
   | ((d: A) => Promise<B | undefined>)
   | ((d: A) => B | undefined);
 
-export const iterate = <A, B>(func: IterFunc<A, B>) => {
-  return async function* (iterable: Iter<A>) {
+export const iterate = <A, B>(func: Func<A, B>) => {
+  return async function* (iterable: AsyncGenerator<A> | AsyncIterable<A>) {
     for await (const item of iterable) {
       const res = await func(item);
       if (res) yield res;
@@ -15,7 +14,7 @@ export const iterate = <A, B>(func: IterFunc<A, B>) => {
   };
 };
 
-export const toArray = async <T>(iterable: Iter<T>) => {
+export const toArray = async <T>(iterable: AsyncGenerator<T> | AsyncIterable<T>) => {
   const result: T[] = [];
   for await (const item of iterable) {
     result.push(item);
