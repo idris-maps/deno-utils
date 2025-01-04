a2b3e634977cbdf2aae6f737b910537e7cc53253
Anders <idris.maps@gmx.ch>
2025-01-04 17:38:50 +0100

    fix(md): expose-options

--- md/to-html/mod.ts
+++ md/to-html/mod.ts
@@ -1,8 +1,13 @@
-import { ParseFlags, ParseOptions, toString } from "./deps.ts";
+import { ParseFlags as Flags, ParseOptions, toString } from "./deps.ts";
 
-export default (source: string | ArrayLike<number>): Promise<string> => {
+export const ParseFlags = Flags;
+
+export default (
+  source: string | ArrayLike<number>,
+  options?: ParseOptions,
+): Promise<string> => {
   const parseOptions: ParseOptions = {
     parseFlags: ParseFlags.DEFAULT | ParseFlags.NO_HTML | ParseFlags.UNDERLINE,
   };
-  return toString(source, parseOptions);
+  return toString(source, options || parseOptions);
 };
